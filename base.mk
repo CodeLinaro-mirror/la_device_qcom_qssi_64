6dc2bbaa49c8cf17255ca84b6310dcc91ac368b9
Taiyab Haque <thaque@qti.qualcomm.com>
2026-04-13 13:17:31 +0530

    qssi_64: enable property ro.camera.default_app_social_media_parity_enabled
    enable property ro.camera.default_app_social_media_parity_enabled
    
    Change-Id: Id4337b0e37b1c00b3d921c04a02dae7caae5fd9d

--- base.mk
+++ base.mk
@@ -886,6 +886,8 @@ PRODUCT_PACKAGES_DEBUG += \
     init.qti.debug-trinket.sh \
     init.qti.debug-bengal.sh \
 
+PRODUCT_PROPERTY_OVERRIDES += ro.camera.default_app_social_media_parity_enabled=true
+
 PRODUCT_PACKAGES += liboemaids_system
 PRODUCT_PACKAGES += liboemaids_vendor
 PRODUCT_PACKAGES += android.hardware.health@2.0-service
